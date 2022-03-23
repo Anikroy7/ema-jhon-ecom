8b3b9ff93220fa08c831a60eefe256a722261e58
Anikroy7 <roy645800@gmail.com>
2022-03-23 16:28:24 +0600

    full product details added with btn

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -6,7 +6,19 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price <small>${price}</small></p>
+                <p className='product-details'>
+
+                    <small>Manufacturer: {seller}</small><br /><br />
+                    <small>Rating: {ratings} star</small>
+
+                </p>
+            </div>
+            <button className='cart-btn'>
+                <h4>Add to cart</h4>
+            </button>
         </div>
     );
 };
